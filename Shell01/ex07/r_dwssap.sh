093eb3580da8029c41eaf9f670f331ca2ca3eea3
Donggeun Lee <donggele@c10r1s5.42seoul.kr>
2021-02-21 21:00:41 +0900

    Shell01 ex07 complete

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^#/d' | awk 'NR%2==0' | sed 's/:.*$//g' | rev | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g'
+cat /etc/passwd | sed '/^#/d' | awk 'NR%2==0' | sed 's/:.*$//g' | rev | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g' | tr -d '\n' 
