9a1e584e2766550f91c2b5c1436701970f6d9330
donggele <dgstudylife@naver.com>
2021-03-09 21:51:05 +0900

    C04 make

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 20:09:25 by donggele          #+#    #+#             */
+/*   Updated: 2021/03/09 20:40:01 by donggele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(int nb)
+{
+	int	div;
+	int	rest;
+	char	c;
+
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else
+	{
+		div = nb / 10;
+		rest = nb % 10;
+		if (div > 0)
+			ft_putnbr(div);
+		c = '0' + rest;
+		write(1, &c, 1);
+	}
+}
