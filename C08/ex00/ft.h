b8140c2206d39c0ba41c297cd112443bfadb7c5d
Donggeun Lee <donggele@c10r1s5.42seoul.kr>
2021-03-15 02:10:06 +0900

    C08 modi

--- C08/ex00/ft.h
+++ C08/ex00/ft.h
@@ -6,7 +6,7 @@
 /*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 19:51:32 by donggele          #+#    #+#             */
-/*   Updated: 2021/03/14 19:52:44 by donggele         ###   ########.fr       */
+/*   Updated: 2021/03/15 01:54:51 by donggele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	ft_putchar(char c);
 void	ft_swap(int *a, int *b);
 void	ft_putstr(char *str);
-int	ft_strlen(char *str);
-int	ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
--- C08/ex03/ft_point.h
+++ C08/ex03/ft_point.h
@@ -6,17 +6,17 @@
 /*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 00:20:04 by donggele          #+#    #+#             */
-/*   Updated: 2021/03/15 01:53:25 by donggele         ###   ########.fr       */
+/*   Updated: 2021/03/15 02:08:45 by donggele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-typedef	struct		t_point
+typedef	struct
 {
 	int x;
 	int y;
-}			t_point;
+}		t_point;
 
 #endif
