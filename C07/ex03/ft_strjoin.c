2c8782524ef52ef9b041c418176e286656aaf923
donggele <dgstudylife@naver.com>
2021-03-15 04:35:35 +0900

    C07 ex03

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -6,13 +6,13 @@
 /*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/14 18:30:32 by donggele          #+#    #+#             */
-/*   Updated: 2021/03/14 19:50:40 by donggele         ###   ########.fr       */
+/*   Updated: 2021/03/15 04:34:19 by donggele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(char *str)
+long long	ft_strlen(char *str)
 {
 	int length;
 
@@ -22,8 +22,38 @@ int	ft_strlen(char *str)
 	return (length);
 }
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+char		*ft_strcpy(char *dest, char *src)
 {
-	
+	while (*src)
+		*dest = *src++;
+	return (dest);
+}
 
+char		*ft_strjoin(int size, char **strs, char *sep)
+{
+	char		*array;
+	char		*temp;
+	long long	strs_len;
+	int		index;
+
+	if (!size)
+	{
+		array = ((char *)malloc(1));
+		array[0] = 0;
+		return (ret);
+	}
+	strs_len = 0;
+	index = 0;
+	while (index < size)
+		strs_len += ft_strlen(strs[index++]);
+	array = (char *)malloc(strs_len + (size - 1) * ft_strlen(sep) + 1);
+	temp = ft_strcpy(array, strs[0]);
+	index = 1;
+	while (index < size)
+	{
+		temp = ft_strcpy(temp, sep);
+		temp = ft_strcpy(temp, strs[index++]);
+	}
+	*temp = 0;
+	return (array);
 }
