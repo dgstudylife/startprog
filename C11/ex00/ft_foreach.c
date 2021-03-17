f0b5db04fb5d128eca8662c88541da775d6887cf
Donggeun Lee <donggele@c8r1s4.42seoul.kr>
2021-03-17 19:52:36 +0900

    c11

--- C11/ex00/ft_foreach.c
+++ C11/ex00/ft_foreach.c
@@ -6,7 +6,7 @@
 /*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 02:33:21 by donggele          #+#    #+#             */
-/*   Updated: 2021/03/17 02:52:35 by donggele         ###   ########.fr       */
+/*   Updated: 2021/03/17 19:47:47 by donggele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@ void	ft_foreach(int *tab, int length, void (*f)(int))
 {
 	int i;
 
-	if (length <= 0)
-		return ;
 	i = 0;
 	while (i < length)
 		f(tab[i++]);
--- C11/ex01/ft_map.c
+++ C11/ex01/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: donggele <donggele@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 02:43:01 by donggele          #+#    #+#             */
-/*   Updated: 2021/03/17 14:36:29 by donggele         ###   ########.fr       */
+/*   Updated: 2021/03/17 19:50:48 by donggele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,6 @@ int	*ft_map(int *tab, int length, int (*f)(int))
 	if (!(array = (int *)malloc(sizeof(int) * length)))
 		return (0);
 	index = 0;
-	if (length <= 0)
-		return (0);
 	while (index < length)
 	{
 		array[index] = f(tab[index]);
